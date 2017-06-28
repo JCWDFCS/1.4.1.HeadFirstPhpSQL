deabf2a5cb84f0e7ebf35eef7fe53cc3a2c1633d
JCWDFCS <342143690@qq.com>
2017-06-28 21:40:38 +0800

    2017-06-28 21:40:35

--- 5.index.php
+++ 5.index.php
@@ -3,7 +3,7 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <title>Guitar Wars - High Scores</title>
-  <link rel="stylesheet" type="text/css" href="style.css" />
+
 </head>
 <body>
   <<h2>Guitar Wars - High Scores</h2>
